023cb36b48bfb1adb9bdfc8742711e9362a498a3
JRockiller1 <Gabriellee26@gmail.com>
2022-06-08 21:25:52 +0800

    added self signed key for SSL(HTTPS)

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from app import app
-# from OpenSSL import SSL
 
+
+# context = SSL.context(SSL.TLSv1_2_METHOD)
+# context.use_certificate('mycert.crt')
+# context.use_privatekey('myprivatekey.key')
 # Adding Secret Key to our App
 app.secret_key = 'make this hard to guess1!'
 
-app.run(debug=True)
+app.run(debug=True,ssl_context=('certificate.pem','key.pem'))
+# app.run(debug=True)
+
+# use openssl generate self signed certificate 
